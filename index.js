28ca11c09804dcd68d639274e9c715f4cd7915ed
MyatThiriKhin <137380130+myatthiri98@users.noreply.github.com>
2023-08-03 10:49:25 +0630

    Add files via upload
    Getting the user's location with JavaScript (Geolocation API)

--- index.js
+++ index.js
@@ -44,3 +44,58 @@ document
       weather.search();
     }
   });
+
+let geocode = {
+  reverseGeocode: function (latitude, longitude) {
+    var api_key = "da452174635a4aa6ae39bade57c17c74";
+    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    var request_url =
+      api_url +
+      "?" +
+      "key=" +
+      api_key +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    var request = new XMLHttpRequest();
+    request.open("GET", request_url, true);
+
+    request.onload = function () {
+      if (request.status === 200) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        weather.fetchWeather(data.results[0].components.city); // print the location
+      } else if (request.status <= 500) {
+        // We reached our target server, but it returned an error
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
+    };
+
+    request.send(); // make the request
+  },
+
+  getlocation: function () {
+    function success(data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    } else {
+      weather.fetchWeather("Seoul");
+    }
+  },
+};
+geocode.getlocation();
